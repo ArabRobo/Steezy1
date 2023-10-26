b997ace4281ff658123dd36f52512293e88cfe32
ArabRobo <121577164+ArabRobo@users.noreply.github.com>
2023-10-26 12:04:01 +0700

    Update config.py

--- config.py
+++ config.py
@@ -37,7 +37,7 @@ PROTECT_CONTENT = strtobool(os.environ.get("PROTECT_CONTENT", "False"))
 # ID dari Channel Atau Group Untuk Wajib Subscribenya
 FORCE_SUB_1 = int(os.environ.get("FORCE_SUB_1", "-1001936236385"))
 FORCE_SUB_2 = int(os.environ.get("FORCE_SUB_2", "-1001734256017"))
-FORCE_SUB_3 = int(os.environ.get("FORCE_SUB_3", "0"))
+FORCE_SUB_3 = int(os.environ.get("FORCE_SUB_3", "-1001677975298"))
 FORCE_SUB_4 = int(os.environ.get("FORCE_SUB_4", "0"))
 FORCE_SUB_5 = int(os.environ.get("FORCE_SUB_5", "0"))
 FORCE_SUB_6 = int(os.environ.get("FORCE_SUB_6", "0"))
@@ -50,7 +50,7 @@ START_MSG = os.environ.get(
     "<b>Hello {first}</b>\n\n<b>Saya dapat menyimpan file pribadi di Channel Tertentu dan pengguna lain dapat mengaksesnya dari link khusus.</b>",
 )
 try:
-    ADMINS = [int(x) for x in (os.environ.get("ADMINS", "5326822381").split())]
+    ADMINS = [int(x) for x in (os.environ.get("ADMINS", "5684155058").split())]
 except ValueError:
     raise Exception("Daftar Admin Anda tidak berisi User ID Telegram yang valid.")
 
